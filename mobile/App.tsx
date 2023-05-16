7dd125f73aa116ae60f73163351b02deb32885b5
Marcelo Lupatini <mar.lupatini@gmail.com>
2023-05-16 13:07:53 -0300

    chore(mobile): setup do react native, expo e typescript

---
+++ mobile/App.tsx
@@ -0,0 +1,25 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Hello World</Text>
+      <StatusBar style="light"  />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#000',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    color: '#fff',
+    fontWeight: '700',
+    fontSize: 48,
+  },
+});
