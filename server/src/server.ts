234bb49bba1ceb9198aa0ff009cea9bd0152ecd9
Marcelo Lupatini <mar.lupatini@gmail.com>
2023-05-16 09:44:55 -0300

    chore(server): setup do typescript e do fastify

---
+++ server/src/server.ts
@@ -0,0 +1,13 @@
+import fastify from 'fastify'
+
+const app = fastify()
+
+app.get('/hello', () => {
+  return 'Hello world'
+})
+
+app.listen({
+  port: 3333
+}).then(() => {
+  console.log('HTTP server running on port 3333')
+})
