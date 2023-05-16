1b4034e6e6a6c662994e548e3b581130e138bc04
Marcelo Lupatini <mar.lupatini@gmail.com>
2023-05-16 09:55:43 -0300

    chore(server): setup do eslint

--- server/src/server.ts
+++ server/src/server.ts
@@ -6,8 +6,10 @@ app.get('/hello', () => {
   return 'Hello world'
 })
 
-app.listen({
-  port: 3333
-}).then(() => {
-  console.log('HTTP server running on port 3333')
-})
+app
+  .listen({
+    port: 3333,
+  })
+  .then(() => {
+    console.log('HTTP server running on port 3333')
+  })
