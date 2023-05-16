eac48422f363a700f0ad4c97d28e00212d548bd8
Marcelo Lupatini <mar.lupatini@gmail.com>
2023-05-16 12:33:21 -0300

    chore(web): setup do react, next, typescript e tailwind

---
+++ web/src/app/page.tsx
@@ -0,0 +1,14 @@
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <div
+      className="
+        text-2xl font-bold uppercase
+        w-24 h-24 m-10 bg-red-400 rounded
+      "
+    >
+      Hello World
+    </div>
+  )
+}
