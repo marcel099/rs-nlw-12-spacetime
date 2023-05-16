d1e26c597b03e49f5b274d8b03e0354d8839cf25
Marcelo Lupatini <mar.lupatini@gmail.com>
2023-05-16 13:37:45 -0300

    chore(mobile): setup do eslint e prettier

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,25 +1,11 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar'
+import { Text, View } from 'react-native'
 
 export default function App() {
   return (
-    <View className="bg-gray-950 flex-1 items-center justify-center">
-      <Text className="text-gray-50 font-bold text-5xl">Hello World</Text>
+    <View className="flex-1 items-center justify-center bg-gray-950">
+      <Text className="text-5xl font-bold text-gray-50">Hello World</Text>
       <StatusBar style="light" translucent />
     </View>
-  );
+  )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#000',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  text: {
-    color: '#fff',
-    fontWeight: '700',
-    fontSize: 48,
-  },
-});
--- mobile/tailwind.config.js
+++ mobile/tailwind.config.js
@@ -1,12 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./App.{js,jsx,ts,tsx}",
-    "./app/**/*.{js,jsx,ts,tsx}"
-  ],
+  content: ['./App.{js,jsx,ts,tsx}', './app/**/*.{js,jsx,ts,tsx}'],
   theme: {
     extend: {},
   },
   plugins: [],
 }
-
