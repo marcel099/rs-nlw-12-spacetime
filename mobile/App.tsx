c76849b15c44a7ec39c08def5d713d18b303c8c6
Marcelo Lupatini <mar.lupatini@gmail.com>
2023-05-17 18:28:54 -0300

    feat(mobile): desenv. da interface Home

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,8 +1,15 @@
 import { StatusBar } from 'expo-status-bar'
-import { Text, View } from 'react-native'
+import { ImageBackground, Text, TouchableOpacity, View } from 'react-native'
 import { useFonts } from 'expo-font'
 import { Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto'
 import { BaiJamjuree_700Bold } from '@expo-google-fonts/bai-jamjuree'
+import { styled } from 'nativewind'
+
+import blurBgImg from './src/shared/assets/bg-blur.png'
+import Stripes from './src/shared/assets/stripes.svg'
+import NlwSpacetimeLogo from './src/shared/assets/nlw-spacetime-logo.svg'
+
+const StyledStripes = styled(Stripes)
 
 export default function App() {
   const [hasLoadedFonts] = useFonts({
@@ -16,9 +23,42 @@ export default function App() {
   }
 
   return (
-    <View className="flex-1 items-center justify-center bg-gray-900">
-      <Text className="font-title text-5xl text-gray-50">Hello World</Text>
+    <ImageBackground
+      source={blurBgImg}
+      className="relative flex-1 items-center bg-gray-900 px-8 py-10"
+      imageStyle={{
+        position: 'absolute',
+        left: '-100%',
+      }}
+    >
       <StatusBar style="light" translucent />
-    </View>
+      <StyledStripes className="absolute left-2" />
+
+      <View className="flex-1 items-center justify-center gap-6">
+        <NlwSpacetimeLogo />
+        <View className="space-y-2">
+          <Text className="text-center font-title text-2xl leading-tight text-gray-50">
+            Sua cápsula do tempo
+          </Text>
+          <Text className="text-center font-body text-base leading-relaxed text-gray-100">
+            Colecione momentos marcantes da sua jornada e compartilhe (se
+            quiser) com o mundo!
+          </Text>
+        </View>
+
+        <TouchableOpacity
+          activeOpacity={0.7}
+          className="rounded-full bg-green-500 px-5 py-2"
+        >
+          <Text className="font-alt text-sm uppercase text-black">
+            Cadastrar lembrança
+          </Text>
+        </TouchableOpacity>
+      </View>
+
+      <Text className="text-center font-body leading-relaxed text-gray-200">
+        Feito com 💜 no NLW da Rocketseat
+      </Text>
+    </ImageBackground>
   )
 }
