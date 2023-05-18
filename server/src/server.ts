5b896f8b378b5a89a6272c226cf91444c0582fa0
Marcelo Lupatini <mar.lupatini@gmail.com>
2023-05-18 12:45:38 -0300

    feat(server): desenv. de rota de autenticação

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,15 +1,23 @@
 import 'dotenv/config'
 import fastify from 'fastify'
 import cors from '@fastify/cors'
+import jwt from '@fastify/jwt'
+
 import { memoriresRoutes } from './routes/memories'
+import { authRoutes } from './routes/auth'
+import { auth } from './config/auth'
 
 const app = fastify()
 
 app.register(cors, {
   origin: true,
 })
+app.register(jwt, {
+  secret: auth.token_secret,
+})
 
 app.register(memoriresRoutes)
+app.register(authRoutes)
 
 app
   .listen({
