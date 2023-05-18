46536ed9ee83b1251ee40bea0a348f6ffa1f1acc
Marcelo Lupatini <mar.lupatini@gmail.com>
2023-05-18 11:11:34 -0300

    feat(server): configuração de cors

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,7 +1,12 @@
 import fastify from 'fastify'
+import cors from '@fastify/cors'
 import { memoriresRoutes } from './routes/memories'
 
 const app = fastify()
+
+app.register(cors, {
+  origin: true,
+})
 
 app.register(memoriresRoutes)
 
