293483ca1526163d078e6966be033531602590aa
Marcelo Lupatini <mar.lupatini@gmail.com>
2023-05-19 10:12:22 -0300

    feat(web): desenv. de interface de nova memória

--- web/src/components/Hero.tsx
+++ web/src/components/Hero.tsx
@@ -1,6 +1,7 @@
 import Image from 'next/image'
 
 import nlwSpacetimeLogo from '@/assets/nlw-spacetime-logo.svg'
+import Link from 'next/link'
 
 export function Hero() {
   return (
@@ -17,12 +18,12 @@ export function Hero() {
         </p>
       </div>
 
-      <a
-        href=""
+      <Link
+        href="/memories/new"
         className="inline-block rounded-full bg-green-500 px-5 py-3 font-alt text-sm uppercase leading-none text-black hover:bg-green-600"
       >
         CADASTRAR LEMBRANÇA
-      </a>
+      </Link>
     </div>
   )
 }
