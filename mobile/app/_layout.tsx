d1755a3056f49537b6e182c069b0741fb6e410d8
Marcelo Lupatini <mar.lupatini@gmail.com>
2023-05-19 11:16:34 -0300

    feat(mobile): redirecionamento para telas protegidas quando autenticado

--- mobile/app/_layout.tsx
+++ mobile/app/_layout.tsx
@@ -1,22 +1,34 @@
 import { Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto'
 import { BaiJamjuree_700Bold } from '@expo-google-fonts/bai-jamjuree'
 import { useFonts } from 'expo-font'
+import * as SecureStore from 'expo-secure-store'
 import { SplashScreen, Stack } from 'expo-router'
 import { StatusBar } from 'expo-status-bar'
 import { styled } from 'nativewind'
+import { useEffect, useState } from 'react'
 import { ImageBackground } from 'react-native'
 
 import blurBgImg from '../src/shared/assets/bg-blur.png'
 import Stripes from '../src/shared/assets/stripes.svg'
+import { SIGNED_IN_USER_TOKEN } from '../src/shared/configs/secureStorage'
 
 const StyledStripes = styled(Stripes)
 
 export default function Layout() {
+  const [isUserAuthenticated, setIsUserAuthenticated] = useState<
+    boolean | null
+  >(null)
   const [hasLoadedFonts] = useFonts({
     Roboto_700Bold,
     Roboto_400Regular,
     BaiJamjuree_700Bold,
   })
+
+  useEffect(() => {
+    SecureStore.getItemAsync(SIGNED_IN_USER_TOKEN).then((token) => {
+      setIsUserAuthenticated(!!token)
+    })
+  }, [])
 
   if (hasLoadedFonts === false) {
     return <SplashScreen />
@@ -41,7 +53,10 @@ export default function Layout() {
             backgroundColor: 'transparent',
           },
         }}
-      />
+      >
+        <Stack.Screen name="index" redirect={isUserAuthenticated} />
+        <Stack.Screen name="memories" />
+      </Stack>
     </ImageBackground>
   )
 }
