ee74ad84fb6f809ddfc68170edbac94ea3e073e4
Marcelo Lupatini <mar.lupatini@gmail.com>
2023-07-24 14:50:32 -0300

    chore(server): preparação da aplicação para ambiente de produção

--- server/src/server.ts
+++ server/src/server.ts
@@ -22,10 +22,6 @@ app.register(multipart, {
   addToBody: true,
   sharedSchemaId: "MultipartFileType",
 })
-app.register(require('@fastify/static'), {
-  root: resolve(__dirname, '../tmp/memory'),
-  prefix: '/uploads/memory',
-})
 
 app.register(memoriresRoutes)
 app.register(authRoutes)
@@ -33,7 +29,7 @@ app.register(uploadRoutes)
 
 app
   .listen({
-    port: 3333,
+    port: Number(process.env.PORT) || 3333,
     host: '0.0.0.0',
   })
   .then(() => {
