709b7a8aea5ff579bcef540fba2f0b17156050da
Marcelo Lupatini <mar.lupatini@gmail.com>
2023-07-24 14:34:10 -0300

    chore(server): preparação do prisma para ambiente de produção

---
+++ server/prisma/migrations/20230724172743_create_table_memories/migration.sql
@@ -0,0 +1,14 @@
+-- CreateTable
+CREATE TABLE "Memory" (
+    "id" TEXT NOT NULL,
+    "coverUrl" TEXT NOT NULL,
+    "content" TEXT NOT NULL,
+    "isPublic" BOOLEAN NOT NULL DEFAULT false,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "userId" TEXT NOT NULL,
+
+    CONSTRAINT "Memory_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "Memory" ADD CONSTRAINT "Memory_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
